2c7e9fe112a1453c168156825620608d17c83993
Vadym Khokhlov <xvadima@ukr.net>
2023-10-25 14:01:03 +0300

    improved api client

--- lib/infrastructure/api/giphy_api_client_impl.dart
+++ lib/infrastructure/api/giphy_api_client_impl.dart
@@ -14,17 +14,39 @@ class GiphyApiClientImpl implements GiphyApiClient {
     required String query,
     required int offset,
     required int limit,
-  }) async {
-    _cancelToken?.cancel();
-    _cancelToken = CancelToken();
-    final result = await _retrofitClient.searchGifs(
-      query,
-      offset,
-      limit,
-      _cancelToken,
-    );
-    _cancelToken = null;
-    return result;
+  }) =>
+      _doRequest(
+        () async {
+          _cancelToken?.cancel();
+          _cancelToken = CancelToken();
+          final result = await _retrofitClient.searchGifs(
+            query,
+            offset,
+            limit,
+            _cancelToken,
+          );
+          _cancelToken = null;
+          return result;
+        },
+      );
+
+  // Here we can log and process any errors and convert its to more
+  // suitable data structures.
+  // F.e. we can process any exceptions on the data layer and pass to the
+  // higher layers Either from the fpdart package
+  Future<T> _doRequest<T>(Future<T> Function() execute) async {
+    try {
+      return await execute();
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.badResponse) {
+        // Parse the 'Bad response' error
+        throw Exception('Bad response');
+      } else {
+        throw Exception('Common network error');
+      }
+    } catch (e) {
+      throw Exception('Unexpected error');
+    }
   }
 
   final RetrofitClient _retrofitClient;
